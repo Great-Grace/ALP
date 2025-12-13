b845fafac09899cac6e0a13670344d70f78072b5
taewoo <xodn3696@naver.com>
2025-12-13 21:03:54 +0900

    Initial Commit

---
+++ ArabicLearning/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  ArabicLearning
+//
+//  Created by 김태우 on 12/13/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ ArabicLearningTests/ArabicLearningTests.swift
@@ -0,0 +1,17 @@
+//
+//  ArabicLearningTests.swift
+//  ArabicLearningTests
+//
+//  Created by 김태우 on 12/13/25.
+//
+
+import Testing
+@testable import ArabicLearning
+
+struct ArabicLearningTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
